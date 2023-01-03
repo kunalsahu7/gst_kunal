2586c7e2989e8627b1eae55f1391e0fda837afa2
RRwW <rw@RWs-Mac-Pro.local>
2023-01-03 10:16:58 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/gst_kunal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gst_kunal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:gst_kunal/Home.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: home(),
+    ),
+  );
+}
